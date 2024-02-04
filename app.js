c590900c8df0b41817e3fdf6e0b5fe5505e6a5af
Nurhalim Habibie <halimnur391@gmail.com>
2024-02-04 21:58:48 +0700

    problem mongodb

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const productRouter = require("./app/product/routes");
-const productRouterV2 = require("./app/product_v2/routes");
+// const productRouter = require("./app/product/routes");
+// const productRouterV2 = require("./app/product_v2/routes");
+const productRouterV3 = require("./app/product_v3/routes");
 const logger = require("morgan");
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "uploads")));
-app.use("/api/v1", productRouter);
-app.use("/api/v2", productRouterV2);
+// app.use("/api/v1", productRouter);
+// app.use("/api/v2", productRouterV2);
+app.use("/api/v3", productRouterV3);
 app.use((req, res, next) => {
   res.status(404);
   res.send({
---
+++ app/product_v3/routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads" });
+const db = require("../../config/mongodb");
+
+router.get("/product", (req, res) => {
+  // res.send("success");
+  db.collection("products")
+    .find()
+    .toArray()
+    .then((result) => res.send(result))
+    .catch((error) => res.send(error));
+});
+
+module.exports = router;
